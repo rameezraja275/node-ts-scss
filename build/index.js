3f365adc969a9102d5a90739147ea64bb5ef6185
rameezraja275 <rameezraja275@gmail.com>
2021-08-08 20:03:38 +0500

    scss added

--- build/index.js
+++ build/index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log('Hello world!');
+var students = [{
+        name: "rameez",
+        age: 27
+    }, {
+        name: "ahmad",
+        age: 30
+    }];
+for (var i = 0; i < students.length; i++) {
+    console.log(students[i].name);
+    console.log(students[i].age);
+}
